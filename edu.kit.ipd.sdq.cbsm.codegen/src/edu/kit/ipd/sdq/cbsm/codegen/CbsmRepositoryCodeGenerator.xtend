77014caa2cf6919a450993c5e0a035d465f9c38a
Jakob Bach <uafmx@student.kit.edu>
2016-06-20 16:45:22 +0200

    Generate code for components
    The code generation logic for Components (and therefore ProvidedRoles,
    RequiredRoles, Repository) is implemented (task 4b and c). Each component
    gets as extra package, has the required roles as fields (inclusive setter
    methods) and the provided roles as super-interfaces whose methods are
    implemented. A helper class is generated for the whole Repository that can
    perform null checks for the methods of the Components. Additional to the
    task, method with return types get a statement which returns a default
    value, so there are no compile errors in that case.

--- edu.kit.ipd.sdq.cbsm.codegen/src/edu/kit/ipd/sdq/cbsm/codegen/CbsmRepositoryCodeGenerator.xtend
+++ edu.kit.ipd.sdq.cbsm.codegen/src/edu/kit/ipd/sdq/cbsm/codegen/CbsmRepositoryCodeGenerator.xtend
@@ -12,6 +12,7 @@ import edu.kit.ipd.sdq.cbsm.repository.Signature
 import edu.kit.ipd.sdq.cbsm.repository.Component
 import edu.kit.ipd.sdq.cbsm.repository.DataType
 import edu.kit.ipd.sdq.cbsm.repository.Parameter
+import java.util.TreeSet
 
 class CbsmRepositoryCodeGenerator implements IGenerator {
 	
@@ -22,12 +23,81 @@ class CbsmRepositoryCodeGenerator implements IGenerator {
 				fsa.generateFile(repository.name + '/' + interfac.name + '.java',
 					interfac.compile(repository.name))
 			]
+			repository.components.forEach[component|
+				fsa.generateFile(component.name + '/' + component.name + 'Impl.java',
+					component.compile(repository.name))
+			]
+			fsa.generateFile(repository.name + '/Helper.java', repository.compile)
 		]
 	}
 	
-	def compile(Component component)  '''
-	
+	def compile(Repository repository) '''
+		package «repository.name»;
+		
+		public class Helper {
+			
+			public static void assertNull(Object object) {
+				assert object == null;
+			}
+			
+			public static void assertNotNull(Object object) {
+				assert object != null;
+			}
+			
+		}
 	'''
+	
+	def compile(Component component, String repositoryName)  '''
+		package «component.name»;
+		
+		«FOR interfaceName: component.uniqueSortedInterfaceNames»
+			import «repositoryName».«interfaceName»;
+		«ENDFOR»
+		«IF !component.requiredRoles.empty»
+			import «repositoryName».Helper;
+		«ENDIF»
+		
+		public class «component.name»Impl «IF !component.providedRoles.empty»implements «FOR providedRole: component.providedRoles SEPARATOR ', '»«providedRole.providedInterface.name»«ENDFOR» «ENDIF»{
+			
+			«FOR requiredRole: component.requiredRoles»
+				private «requiredRole.requiredInterface.name» «requiredRole.requiredInterface.name.toFirstLower»;
+			«ENDFOR»
+			
+			«FOR requiredRole: component.requiredRoles SEPARATOR '\n'»
+				public void set«requiredRole.requiredInterface.name»(«requiredRole.requiredInterface.name» «requiredRole.requiredInterface.name.toFirstLower») {
+					Helper.assertNull(this.«requiredRole.requiredInterface.name.toFirstLower»);
+					this.«requiredRole.requiredInterface.name.toFirstLower» = «requiredRole.requiredInterface.name.toFirstLower»;
+				}
+			«ENDFOR»
+			
+			«FOR providedRole: component.providedRoles SEPARATOR '\n'»
+				«FOR signature: providedRole.providedInterface.signatures SEPARATOR '\n'»
+					//Implementing «signature.name» from interface «providedRole.providedInterface.name»
+					@Override
+					public «signature.compile» {
+						«FOR requiredRole: component.requiredRoles»
+							Helper.assertNotNull(this.«requiredRole.requiredInterface.name.toFirstLower»);
+						«ENDFOR»
+						// TODO: Insert code here
+						«IF signature.returnType != null»
+							return «signature.returnType.defaultReturnValue»;
+						«ENDIF»
+					}
+				«ENDFOR»
+			«ENDFOR»
+		}
+	'''
+	
+	def uniqueSortedInterfaceNames(Component component) {
+		val result = new TreeSet<String>
+		component.providedRoles.forEach[providedRole|
+			result += providedRole.providedInterface.name
+		]
+		component.requiredRoles.forEach[requiredRole|
+			result += requiredRole.requiredInterface.name
+		]
+		return result
+	}
 	
 	def compile(Interface interfac, String repositoryName) '''
 		package «repositoryName»;
@@ -35,15 +105,14 @@ class CbsmRepositoryCodeGenerator implements IGenerator {
 		public interface «interfac.name» {
 			
 			«FOR signature: interfac.signatures»
-				«signature.compile»
+				«signature.compile»;
 				
 			«ENDFOR»
 		}
 	'''
 	
 	def compile(Signature signature) '''
-		«signature.returnType.compile» «signature.name»(«FOR parameter: signature.parameters SEPARATOR ', '»«parameter.compile»«ENDFOR»);
-	'''
+		«signature.returnType.compile» «signature.name»(«FOR parameter: signature.parameters SEPARATOR ', '»«parameter.compile»«ENDFOR»)'''
 	
 	def compile(Parameter parameter) '''
 		«parameter.type.compile» «parameter.name»'''
@@ -61,10 +130,31 @@ class CbsmRepositoryCodeGenerator implements IGenerator {
 				case DOUBLE: 'double'
 				case FLOAT: 'float'
 				case INT: 'int'
-				case LIST: 'java.util.List'
+				case LIST: 'java.util.List<?>'
 				case LONG: 'long'
-				case MAP: 'java.util.Map'
-				case STRING: 'String'
+				case MAP: 'java.util.Map<?,?>'
+				case STRING: 'java.lang.String'
+			}
+		}
+	}
+	
+	def defaultReturnValue(DataType dataType) {
+		if (dataType == null) {
+			return ''
+		} else if (dataType instanceof ComplexType) {
+			return 'null'
+		} else if (dataType instanceof SimpleType) {
+			switch dataType.typeInstance {
+				case BOOLEAN: 'false'
+				case CHAR: '0'
+				case DATE: 'null'
+				case DOUBLE: '0.0'
+				case FLOAT: '0.0f'
+				case INT: '0'
+				case LIST: 'null'
+				case LONG: '0L'
+				case MAP: 'null'
+				case STRING: 'null'
 			}
 		}
 	}
--- edu.kit.ipd.sdq.cbsm.codegen/xtend-gen/edu/kit/ipd/sdq/cbsm/codegen/CbsmRepositoryCodeGenerator.java
+++ edu.kit.ipd.sdq.cbsm.codegen/xtend-gen/edu/kit/ipd/sdq/cbsm/codegen/CbsmRepositoryCodeGenerator.java
@@ -7,10 +7,13 @@ import edu.kit.ipd.sdq.cbsm.repository.Component;
 import edu.kit.ipd.sdq.cbsm.repository.DataType;
 import edu.kit.ipd.sdq.cbsm.repository.Interface;
 import edu.kit.ipd.sdq.cbsm.repository.Parameter;
+import edu.kit.ipd.sdq.cbsm.repository.ProvidedRole;
 import edu.kit.ipd.sdq.cbsm.repository.Repository;
+import edu.kit.ipd.sdq.cbsm.repository.RequiredRole;
 import edu.kit.ipd.sdq.cbsm.repository.Signature;
 import edu.kit.ipd.sdq.cbsm.repository.SimpleType;
 import edu.kit.ipd.sdq.cbsm.repository.SimpleTypeInstance;
+import java.util.TreeSet;
 import java.util.function.Consumer;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
@@ -19,6 +22,7 @@ import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.xtend2.lib.StringConcatenation;
 import org.eclipse.xtext.generator.IFileSystemAccess;
 import org.eclipse.xtext.generator.IGenerator;
+import org.eclipse.xtext.xbase.lib.StringExtensions;
 
 @SuppressWarnings("all")
 public class CbsmRepositoryCodeGenerator implements IGenerator {
@@ -40,15 +44,299 @@ public class CbsmRepositoryCodeGenerator implements IGenerator {
         fsa.generateFile(_plus_2, _compile);
       };
       _interfaces.forEach(_function_1);
+      EList<Component> _components = repository.getComponents();
+      final Consumer<Component> _function_2 = (Component component) -> {
+        String _name = component.getName();
+        String _plus = (_name + "/");
+        String _name_1 = component.getName();
+        String _plus_1 = (_plus + _name_1);
+        String _plus_2 = (_plus_1 + "Impl.java");
+        String _name_2 = repository.getName();
+        CharSequence _compile = this.compile(component, _name_2);
+        fsa.generateFile(_plus_2, _compile);
+      };
+      _components.forEach(_function_2);
+      String _name = repository.getName();
+      String _plus = (_name + "/Helper.java");
+      CharSequence _compile = this.compile(repository);
+      fsa.generateFile(_plus, _compile);
     };
     _filter.forEach(_function);
   }
   
-  public CharSequence compile(final Component component) {
+  public CharSequence compile(final Repository repository) {
     StringConcatenation _builder = new StringConcatenation();
+    _builder.append("package ");
+    String _name = repository.getName();
+    _builder.append(_name, "");
+    _builder.append(";");
+    _builder.newLineIfNotEmpty();
+    _builder.newLine();
+    _builder.append("public class Helper {");
+    _builder.newLine();
     _builder.append("\t");
     _builder.newLine();
+    _builder.append("\t");
+    _builder.append("public static void assertNull(Object object) {");
+    _builder.newLine();
+    _builder.append("\t\t");
+    _builder.append("assert object == null;");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("}");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("public static void assertNotNull(Object object) {");
+    _builder.newLine();
+    _builder.append("\t\t");
+    _builder.append("assert object != null;");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("}");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.newLine();
+    _builder.append("}");
+    _builder.newLine();
     return _builder;
+  }
+  
+  public CharSequence compile(final Component component, final String repositoryName) {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("package ");
+    String _name = component.getName();
+    _builder.append(_name, "");
+    _builder.append(";");
+    _builder.newLineIfNotEmpty();
+    _builder.newLine();
+    {
+      TreeSet<String> _uniqueSortedInterfaceNames = this.uniqueSortedInterfaceNames(component);
+      for(final String interfaceName : _uniqueSortedInterfaceNames) {
+        _builder.append("import ");
+        _builder.append(repositoryName, "");
+        _builder.append(".");
+        _builder.append(interfaceName, "");
+        _builder.append(";");
+        _builder.newLineIfNotEmpty();
+      }
+    }
+    {
+      EList<RequiredRole> _requiredRoles = component.getRequiredRoles();
+      boolean _isEmpty = _requiredRoles.isEmpty();
+      boolean _not = (!_isEmpty);
+      if (_not) {
+        _builder.append("import ");
+        _builder.append(repositoryName, "");
+        _builder.append(".Helper;");
+        _builder.newLineIfNotEmpty();
+      }
+    }
+    _builder.newLine();
+    _builder.append("public class ");
+    String _name_1 = component.getName();
+    _builder.append(_name_1, "");
+    _builder.append("Impl ");
+    {
+      EList<ProvidedRole> _providedRoles = component.getProvidedRoles();
+      boolean _isEmpty_1 = _providedRoles.isEmpty();
+      boolean _not_1 = (!_isEmpty_1);
+      if (_not_1) {
+        _builder.append("implements ");
+        {
+          EList<ProvidedRole> _providedRoles_1 = component.getProvidedRoles();
+          boolean _hasElements = false;
+          for(final ProvidedRole providedRole : _providedRoles_1) {
+            if (!_hasElements) {
+              _hasElements = true;
+            } else {
+              _builder.appendImmediate(", ", "");
+            }
+            Interface _providedInterface = providedRole.getProvidedInterface();
+            String _name_2 = _providedInterface.getName();
+            _builder.append(_name_2, "");
+          }
+        }
+        _builder.append(" ");
+      }
+    }
+    _builder.append("{");
+    _builder.newLineIfNotEmpty();
+    _builder.append("\t");
+    _builder.newLine();
+    {
+      EList<RequiredRole> _requiredRoles_1 = component.getRequiredRoles();
+      for(final RequiredRole requiredRole : _requiredRoles_1) {
+        _builder.append("\t");
+        _builder.append("private ");
+        Interface _requiredInterface = requiredRole.getRequiredInterface();
+        String _name_3 = _requiredInterface.getName();
+        _builder.append(_name_3, "\t");
+        _builder.append(" ");
+        Interface _requiredInterface_1 = requiredRole.getRequiredInterface();
+        String _name_4 = _requiredInterface_1.getName();
+        String _firstLower = StringExtensions.toFirstLower(_name_4);
+        _builder.append(_firstLower, "\t");
+        _builder.append(";");
+        _builder.newLineIfNotEmpty();
+      }
+    }
+    _builder.append("\t");
+    _builder.newLine();
+    {
+      EList<RequiredRole> _requiredRoles_2 = component.getRequiredRoles();
+      boolean _hasElements_1 = false;
+      for(final RequiredRole requiredRole_1 : _requiredRoles_2) {
+        if (!_hasElements_1) {
+          _hasElements_1 = true;
+        } else {
+          _builder.appendImmediate("\n", "\t");
+        }
+        _builder.append("\t");
+        _builder.append("public void set");
+        Interface _requiredInterface_2 = requiredRole_1.getRequiredInterface();
+        String _name_5 = _requiredInterface_2.getName();
+        _builder.append(_name_5, "\t");
+        _builder.append("(");
+        Interface _requiredInterface_3 = requiredRole_1.getRequiredInterface();
+        String _name_6 = _requiredInterface_3.getName();
+        _builder.append(_name_6, "\t");
+        _builder.append(" ");
+        Interface _requiredInterface_4 = requiredRole_1.getRequiredInterface();
+        String _name_7 = _requiredInterface_4.getName();
+        String _firstLower_1 = StringExtensions.toFirstLower(_name_7);
+        _builder.append(_firstLower_1, "\t");
+        _builder.append(") {");
+        _builder.newLineIfNotEmpty();
+        _builder.append("\t");
+        _builder.append("\t");
+        _builder.append("Helper.assertNull(this.");
+        Interface _requiredInterface_5 = requiredRole_1.getRequiredInterface();
+        String _name_8 = _requiredInterface_5.getName();
+        String _firstLower_2 = StringExtensions.toFirstLower(_name_8);
+        _builder.append(_firstLower_2, "\t\t");
+        _builder.append(");");
+        _builder.newLineIfNotEmpty();
+        _builder.append("\t");
+        _builder.append("\t");
+        _builder.append("this.");
+        Interface _requiredInterface_6 = requiredRole_1.getRequiredInterface();
+        String _name_9 = _requiredInterface_6.getName();
+        String _firstLower_3 = StringExtensions.toFirstLower(_name_9);
+        _builder.append(_firstLower_3, "\t\t");
+        _builder.append(" = ");
+        Interface _requiredInterface_7 = requiredRole_1.getRequiredInterface();
+        String _name_10 = _requiredInterface_7.getName();
+        String _firstLower_4 = StringExtensions.toFirstLower(_name_10);
+        _builder.append(_firstLower_4, "\t\t");
+        _builder.append(";");
+        _builder.newLineIfNotEmpty();
+        _builder.append("\t");
+        _builder.append("}");
+        _builder.newLine();
+      }
+    }
+    _builder.append("\t");
+    _builder.newLine();
+    {
+      EList<ProvidedRole> _providedRoles_2 = component.getProvidedRoles();
+      boolean _hasElements_2 = false;
+      for(final ProvidedRole providedRole_1 : _providedRoles_2) {
+        if (!_hasElements_2) {
+          _hasElements_2 = true;
+        } else {
+          _builder.appendImmediate("\n", "\t");
+        }
+        {
+          Interface _providedInterface_1 = providedRole_1.getProvidedInterface();
+          EList<Signature> _signatures = _providedInterface_1.getSignatures();
+          boolean _hasElements_3 = false;
+          for(final Signature signature : _signatures) {
+            if (!_hasElements_3) {
+              _hasElements_3 = true;
+            } else {
+              _builder.appendImmediate("\n", "\t");
+            }
+            _builder.append("\t");
+            _builder.append("//Implementing ");
+            String _name_11 = signature.getName();
+            _builder.append(_name_11, "\t");
+            _builder.append(" from interface ");
+            Interface _providedInterface_2 = providedRole_1.getProvidedInterface();
+            String _name_12 = _providedInterface_2.getName();
+            _builder.append(_name_12, "\t");
+            _builder.newLineIfNotEmpty();
+            _builder.append("\t");
+            _builder.append("@Override");
+            _builder.newLine();
+            _builder.append("\t");
+            _builder.append("public ");
+            CharSequence _compile = this.compile(signature);
+            _builder.append(_compile, "\t");
+            _builder.append(" {");
+            _builder.newLineIfNotEmpty();
+            {
+              EList<RequiredRole> _requiredRoles_3 = component.getRequiredRoles();
+              for(final RequiredRole requiredRole_2 : _requiredRoles_3) {
+                _builder.append("\t");
+                _builder.append("\t");
+                _builder.append("Helper.assertNotNull(this.");
+                Interface _requiredInterface_8 = requiredRole_2.getRequiredInterface();
+                String _name_13 = _requiredInterface_8.getName();
+                String _firstLower_5 = StringExtensions.toFirstLower(_name_13);
+                _builder.append(_firstLower_5, "\t\t");
+                _builder.append(");");
+                _builder.newLineIfNotEmpty();
+              }
+            }
+            _builder.append("\t");
+            _builder.append("\t");
+            _builder.append("// TODO: Insert code here");
+            _builder.newLine();
+            {
+              DataType _returnType = signature.getReturnType();
+              boolean _notEquals = (!Objects.equal(_returnType, null));
+              if (_notEquals) {
+                _builder.append("\t");
+                _builder.append("\t");
+                _builder.append("return ");
+                DataType _returnType_1 = signature.getReturnType();
+                String _defaultReturnValue = this.defaultReturnValue(_returnType_1);
+                _builder.append(_defaultReturnValue, "\t\t");
+                _builder.append(";");
+                _builder.newLineIfNotEmpty();
+              }
+            }
+            _builder.append("\t");
+            _builder.append("}");
+            _builder.newLine();
+          }
+        }
+      }
+    }
+    _builder.append("}");
+    _builder.newLine();
+    return _builder;
+  }
+  
+  public TreeSet<String> uniqueSortedInterfaceNames(final Component component) {
+    final TreeSet<String> result = new TreeSet<String>();
+    EList<ProvidedRole> _providedRoles = component.getProvidedRoles();
+    final Consumer<ProvidedRole> _function = (ProvidedRole providedRole) -> {
+      Interface _providedInterface = providedRole.getProvidedInterface();
+      String _name = _providedInterface.getName();
+      result.add(_name);
+    };
+    _providedRoles.forEach(_function);
+    EList<RequiredRole> _requiredRoles = component.getRequiredRoles();
+    final Consumer<RequiredRole> _function_1 = (RequiredRole requiredRole) -> {
+      Interface _requiredInterface = requiredRole.getRequiredInterface();
+      String _name = _requiredInterface.getName();
+      result.add(_name);
+    };
+    _requiredRoles.forEach(_function_1);
+    return result;
   }
   
   public CharSequence compile(final Interface interfac, final String repositoryName) {
@@ -71,6 +359,7 @@ public class CbsmRepositoryCodeGenerator implements IGenerator {
         _builder.append("\t");
         CharSequence _compile = this.compile(signature);
         _builder.append(_compile, "\t");
+        _builder.append(";");
         _builder.newLineIfNotEmpty();
         _builder.append("\t");
         _builder.newLine();
@@ -103,8 +392,7 @@ public class CbsmRepositoryCodeGenerator implements IGenerator {
         _builder.append(_compile_1, "");
       }
     }
-    _builder.append(");");
-    _builder.newLineIfNotEmpty();
+    _builder.append(")");
     return _builder;
   }
   
@@ -154,16 +442,75 @@ public class CbsmRepositoryCodeGenerator implements IGenerator {
                 _switchResult = "int";
                 break;
               case LIST:
-                _switchResult = "java.util.List";
+                _switchResult = "java.util.List<?>";
                 break;
               case LONG:
                 _switchResult = "long";
                 break;
               case MAP:
-                _switchResult = "java.util.Map";
+                _switchResult = "java.util.Map<?,?>";
                 break;
               case STRING:
-                _switchResult = "String";
+                _switchResult = "java.lang.String";
+                break;
+              default:
+                break;
+            }
+          }
+          _xifexpression_2 = _switchResult;
+        }
+        _xifexpression_1 = _xifexpression_2;
+      }
+      _xifexpression = _xifexpression_1;
+    }
+    return _xifexpression;
+  }
+  
+  public String defaultReturnValue(final DataType dataType) {
+    String _xifexpression = null;
+    boolean _equals = Objects.equal(dataType, null);
+    if (_equals) {
+      return "";
+    } else {
+      String _xifexpression_1 = null;
+      if ((dataType instanceof ComplexType)) {
+        return "null";
+      } else {
+        String _xifexpression_2 = null;
+        if ((dataType instanceof SimpleType)) {
+          String _switchResult = null;
+          SimpleTypeInstance _typeInstance = ((SimpleType)dataType).getTypeInstance();
+          if (_typeInstance != null) {
+            switch (_typeInstance) {
+              case BOOLEAN:
+                _switchResult = "false";
+                break;
+              case CHAR:
+                _switchResult = "0";
+                break;
+              case DATE:
+                _switchResult = "null";
+                break;
+              case DOUBLE:
+                _switchResult = "0.0";
+                break;
+              case FLOAT:
+                _switchResult = "0.0f";
+                break;
+              case INT:
+                _switchResult = "0";
+                break;
+              case LIST:
+                _switchResult = "null";
+                break;
+              case LONG:
+                _switchResult = "0L";
+                break;
+              case MAP:
+                _switchResult = "null";
+                break;
+              case STRING:
+                _switchResult = "null";
                 break;
               default:
                 break;
